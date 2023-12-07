68c422e97d996d398215fe35c71c64e9b3b8682d
DiegoDalera <dmdalera@gmail.com>
2023-12-07 08:56:27 -0300

    footer container

--- src/components/FooterContainer.jsx
+++ src/components/FooterContainer.jsx
@@ -3,46 +3,174 @@ import { Github, Linkedin, Instagram } from "react-bootstrap-icons";
 
 const FooterContainer = () => {
   return (
-    <div className="footer" id="footer">
-      <div className="footer-info">
-        <p>Diego Dalera</p>
+    <footer className="footer-section">
+      <div className="container">
+        <div className="footer-cta">
+          <div className="footer-row">
+            <div className="single-cta">
+              <i className="fas fa-map-marker-alt"></i>
+              <div className="cta-text">
+                <h4>Find us</h4>
+                <span>1010 Avenue, sw 54321, chandigarh</span>
+              </div>
+            </div>
 
-        <div className="footer-menu">
-          <p>CABA - Argentina</p>
-          <p>011-59738326 - dmdalera@gmail.com</p>
+            <div className="single-cta">
+              <i className="fas fa-phone"></i>
+              <div className="cta-text">
+                <h4>Call us</h4>
+                <span>9876543210 0</span>
+              </div>
+            </div>
+
+            <div className="single-cta">
+              <i className="far fa-envelope-open"></i>
+              <div className="cta-text">
+                <h4>Mail us</h4>
+                <span>mail@info.com</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-content">
+          <div className="footer-row">
+            <div className="footer-widget">
+              <div className="footer-logo">
+                <a href="index.html">
+                  <img
+                    src="https://i.ibb.co/QDy827D/ak-logo.png"
+                    className="img-fluid"
+                    alt="logo"
+                  ></img>
+                </a>
+              </div>
+              <div className="footer-text">
+                <p>
+                  Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed
+                  do eiusmod tempor incididuntut consec tetur adipisicing
+                  elit,Lorem ipsum dolor sit amet.
+                </p>
+              </div>
+
+              <div className="footer-social-icon">
+                <span>Follow us</span>
+                <a href="#">
+                  <i className="fab fa-facebook-f facebook-bg"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-twitter twitter-bg"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-google-plus-g google-bg"></i>
+                </a>
+              </div>
+            </div>
+
+           
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Useful Links</h3>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">about</a>
+                  </li>
+                  <li>
+                    <a href="#">services</a>
+                  </li>
+                  <li>
+                    <a href="#">portfolio</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                  <li>
+                    <a href="#">About us</a>
+                  </li>
+                  <li>
+                    <a href="#">Our Services</a>
+                  </li>
+                  <li>
+                    <a href="#">Expert Team</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact us</a>
+                  </li>
+                  <li>
+                    <a href="#">Latest News</a>
+                  </li>
+                </ul>
+              </div>
+           
+
+           
+           
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Subscribe</h3>
+                </div>
+                <div className="footer-text mb-25">
+                  <p>
+                    Don’t miss to subscribe to our new feeds, kindly fill the
+                    form below.
+                  </p>
+                </div>
+                <div className="subscribe-form">
+                  <form action="#">
+                    <input type="text" placeholder="Email Address"></input>
+                    <button>
+                      <i className="fab fa-telegram-plane"></i>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            
+            
+
+          </div>
         </div>
       </div>
-
-      <ul className="redes_sociales">
-        <li>
-          <a
-            href="https://github.com/DiegoDalera"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Github className="footer-icon" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/diego-martin-dalera/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Linkedin className="footer-icon" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/diegomartin.arg"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Instagram className="footer-icon" />
-          </a>
-        </li>
-      </ul>
-    </div>
+      
+      <div className="copyright-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+              <div className="copyright-text">
+                <p>
+                  Copyright &copy; 2018, All Right Reserved{" "}
+                  <a href="https://codepen.io/anupkumar92/">Anup</a>
+                </p>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy</a>
+                  </li>
+                  <li>
+                    <a href="#">Policy</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
