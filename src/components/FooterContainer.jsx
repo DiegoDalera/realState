c26edf6d058e67658bf0607c0d67d5ad3549c133
DiegoDalera <dmdalera@gmail.com>
2023-12-07 09:46:54 -0300

    social react icons

--- src/components/FooterContainer.jsx
+++ src/components/FooterContainer.jsx
@@ -1,5 +1,12 @@
 import "./FooterContainer.css";
-import { Github, Linkedin, Instagram } from "react-bootstrap-icons";
+import {
+  Github,
+  Linkedin,
+  Instagram,
+  GeoFill,
+  PhoneFill,
+  EnvelopeFill,
+} from "react-bootstrap-icons";
 
 const FooterContainer = () => {
   return (
@@ -10,23 +17,26 @@ const FooterContainer = () => {
             <div className="single-cta">
               <i className="fas fa-map-marker-alt"></i>
               <div className="cta-text">
-                <h4>Find us</h4>
-                <span>1010 Avenue, sw 54321, chandigarh</span>
+                <GeoFill color="white" size={36} />
+                <h4>Nuestra Ubicacion</h4>
+                <span>Barcarce 500 - CABA - ARGENTINA</span>
               </div>
             </div>
 
             <div className="single-cta">
               <i className="fas fa-phone"></i>
               <div className="cta-text">
-                <h4>Call us</h4>
-                <span>9876543210 0</span>
+                <PhoneFill color="white" size={36} />
+                <h4>Telefono</h4>
+                <span>+051 117654321</span>
               </div>
             </div>
 
             <div className="single-cta">
               <i className="far fa-envelope-open"></i>
               <div className="cta-text">
-                <h4>Mail us</h4>
+                <EnvelopeFill color="white" size={36} />
+                <h4>E mail</h4>
                 <span>mail@info.com</span>
               </div>
             </div>
@@ -52,9 +62,15 @@ const FooterContainer = () => {
                   elit,Lorem ipsum dolor sit amet.
                 </p>
               </div>
+            </div>
+
+            <div className="footer-widget">
+              
+              <div className="footer-widget-heading">
+                <h3>Follow us</h3>
+              </div>
 
               <div className="footer-social-icon">
-                <span>Follow us</span>
                 <a href="#">
                   <i className="fab fa-facebook-f facebook-bg"></i>
                 </a>
@@ -65,107 +81,57 @@ const FooterContainer = () => {
                   <i className="fab fa-google-plus-g google-bg"></i>
                 </a>
               </div>
+
             </div>
 
-           
-              <div className="footer-widget">
-                <div className="footer-widget-heading">
-                  <h3>Useful Links</h3>
-                </div>
-                <ul>
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">about</a>
-                  </li>
-                  <li>
-                    <a href="#">services</a>
-                  </li>
-                  <li>
-                    <a href="#">portfolio</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact</a>
-                  </li>
-                  <li>
-                    <a href="#">About us</a>
-                  </li>
-                  <li>
-                    <a href="#">Our Services</a>
-                  </li>
-                  <li>
-                    <a href="#">Expert Team</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact us</a>
-                  </li>
-                  <li>
-                    <a href="#">Latest News</a>
-                  </li>
-                </ul>
+            <div className="footer-widget">
+              <div className="footer-widget-heading">
+                <h3>Subscribe</h3>
               </div>
-           
-
-           
-           
-              <div className="footer-widget">
-                <div className="footer-widget-heading">
-                  <h3>Subscribe</h3>
-                </div>
-                <div className="footer-text mb-25">
-                  <p>
-                    Don’t miss to subscribe to our new feeds, kindly fill the
-                    form below.
-                  </p>
-                </div>
-                <div className="subscribe-form">
-                  <form action="#">
-                    <input type="text" placeholder="Email Address"></input>
-                    <button>
-                      <i className="fab fa-telegram-plane"></i>
-                    </button>
-                  </form>
-                </div>
+              <div className="footer-text mb-25">
+                <p>
+                  Don’t miss to subscribe to our new feeds, kindly fill the form
+                  below.
+                </p>
               </div>
-            
-            
-
+              <div className="subscribe-form">
+                <form action="#">
+                  <input type="text" placeholder="Email Address"></input>
+                  <button>
+                    <i className="fab fa-telegram-plane"></i>
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      
+
       <div className="copyright-area">
         <div className="container">
-          <div className="row">
-            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
-              <div className="copyright-text">
-                <p>
-                  Copyright &copy; 2018, All Right Reserved{" "}
-                  <a href="https://codepen.io/anupkumar92/">Anup</a>
-                </p>
-              </div>
+          <div className="footer-row">
+            <div className="copyright-text">
+              <p>Copyright &copy; 2018, All Right Reserved </p>
             </div>
-            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-              <div className="footer-menu">
-                <ul>
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">Terms</a>
-                  </li>
-                  <li>
-                    <a href="#">Privacy</a>
-                  </li>
-                  <li>
-                    <a href="#">Policy</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact</a>
-                  </li>
-                </ul>
-              </div>
+
+            <div className="footer-menu">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Terms</a>
+                </li>
+                <li>
+                  <a href="#">Privacy</a>
+                </li>
+                <li>
+                  <a href="#">Policy</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
