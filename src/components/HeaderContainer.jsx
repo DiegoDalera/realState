587de4ed80b137493a2e694acc82409f90771585
DiegoDalera <dmdalera@gmail.com>
2023-12-08 11:24:50 -0300

    range form

--- src/components/HeaderContainer.jsx
+++ src/components/HeaderContainer.jsx
@@ -1,12 +1,22 @@
 import NavContainer from "./NavContainer";
 import "./HeaderContainer.css";
+import { useState } from "react";
 
 const HeaderContainer = () => {
+  const [valorRango, setValorRango] = useState(0);
+
+  // Manejador para actualizar el estado cuando el rango cambia
+  const handleRangoChange = (event) => {
+    setValorRango(event.target.value);
+  };
+
   return (
     <header className="header">
-      <NavContainer />
+      <div className="nav-container">
+        <NavContainer />
+      </div>
+
       <div className="header-container">
-        
         <div className="header-title">
           <h1> La propiedad de tus sueños</h1>
           <p>
@@ -21,84 +31,64 @@ const HeaderContainer = () => {
           <form className="formContainer">
             <div className="formFormGroup">
               <select className="formSelect">
-                <option className="formOption">Types</option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  rentals
-                </option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  Sales
-                </option>
+                <option className="formOption">Tipo de operacion</option>
+                <option className="formOption">Alquiler</option>
+                <option className="formOption">Venta</option>
+                <option className="formOption">Alquiler temporario</option>
               </select>
             </div>
 
             <div className="formFormGroup">
               <select className="formSelect">
-                <option className="formOption">Types</option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  rentals
-                </option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  Sales
-                </option>
+                <option className="formOption">Tipo de</option>
+                <option className="formOption">Casa</option>
+                <option className="formOption">Departamento</option>
               </select>
             </div>
 
             <div className="formFormGroup">
               <select className="formSelect">
-                <option className="formOption">Types</option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  rentals
-                </option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  Sales
-                </option>
+                <option className="formOption">Ubicacion</option>
+                <option className="formOption">Capital Federal</option>
+                <option className="formOption">Bs As</option>
+                <option className="formOption">Cordoba</option>
               </select>
             </div>
 
             <div className="formFormGroup">
               <select className="formSelect">
-                <option className="formOption">Types</option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  rentals
-                </option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  Sales
-                </option>
+                <option className="formOption">Ambientes</option>
+                <option className="formOption">+1</option>
+                <option className="formOption">+2</option>
+                <option className="formOption">+3</option>
               </select>
             </div>
 
             <div className="formFormGroup">
-              <select className="formSelect">
-                <option className="formOption">Types</option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  rentals
-                </option>
-                <option className="form__Option-sc-6kozr0-3 bmgawS">
-                  Sales
-                </option>
-              </select>
-            </div>
-
-            <div className="formFormGroup">
-              <input
-                type="range"
-                min="25000"
-                max="350000"
-                className="form__RangeInput"
-                value="162887"
-              ></input>
+              <div className="inputrange">
+                <label className="formRange" htmlFor="rangoPrecio">
+                  Precio Maximo ${valorRango}
+                </label>
+                <input
+                  type="range"
+                  id="rangoPrecio"
+                  name="rangoPrecio"
+                  min="0"
+                  max="1000000"
+                  value={valorRango}
+                  onChange={handleRangoChange}
+                />
+              </div>
             </div>
 
             <div className="formFormGroup">
               <input
                 type="submit"
                 className="formSubmitInput"
-                value="Search"
+                value="Buscar"
               ></input>
-            
             </div>
           </form>
-
         </div>
       </div>
     </header>
